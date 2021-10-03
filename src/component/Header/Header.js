dc5254b8ef86041bee4ee38acf30dbae71132ef5
As-Shovo <akbarshovo18@gmail.com>
2021-10-03 20:57:32 +0600

    responsive

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -6,17 +6,19 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Navbar bg="dark" variant="dark" >
-                <Container>
-                    <Nav className="mx-auto">
-                        <div className='navbar-menu'> <Link to="/home">Home</Link> </div>    
-                        <div className='navbar-menu'> <Link to="/juicyBar">Juicy Bar</Link> </div>    
-                        <div className='navbar-menu'> <Link to="/about">About</Link> </div>  
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Awasom Jucie Bar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <div className='navbar-menu'> <Link to="/home">Home</Link> </div>
+                        <div className='navbar-menu'> <Link to="/juicyBar">Juicy Bar</Link> </div>
+                        <div className='navbar-menu'> <Link to="/about">About</Link> </div>
                     </Nav>
-                </Container>
-            </Navbar>
-        </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 import './Home.css'
 const Home = () => {
     return (
         <div className="home">
-            <div className="home-info">
-                <h1>Welcome</h1>
-                <p>The Best Juicy Bar In Web</p>
-            </div>
-            
+            <Container>
+                <div className="home-info">
+                    <h1>Welcome</h1>
+                    <p>The Best Juicy Bar In Web</p>
+                </div>
+            </Container>
+
         </div>
     );
 };
